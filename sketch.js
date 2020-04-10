6afc98b3bf99a886bf03c9a7fc0ed8184bbee4d2
siddhant15oo <61053704+siddhant15oo@users.noreply.github.com>
2020-04-10 19:24:57 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,58 @@
+var snake
+var rez = 20;
+var food;
+var w;
+var h;
+
+function setup() {
+  createCanvas(400, 400);
+  w = floor(width / rez);
+  h = floor(height / rez);
+  frameRate(5);
+  snake=new Snake();
+  foodLocation();
+}
+
+function foodLocation() {
+  var x = floor(random(w));
+  var y = floor(random(h));
+  food = createVector(x, y);
+
+}
+
+function keyPressed() {
+  if (keyCode === LEFT_ARROW) {
+    snake.setDir(-1, 0);
+  } else if (keyCode === RIGHT_ARROW) {
+    snake.setDir(1, 0);
+  } else if (keyCode === DOWN_ARROW) {
+    snake.setDir(0, 1);
+  } else if (keyCode === UP_ARROW) {
+    snake.setDir(0, -1);
+  } else if (key == ' ') {
+    snake.grow();
+  }
+
+}
+
+function draw() {
+  scale(rez);
+  background(200);
+  if (snake.eat(food)) {
+    foodLocation();
+  }
+  snake.update();
+  snake.display();
+
+
+  if (snake.endGame()) {
+    background(0, 200, 200);
+    noLoop();
+    
+  }
+
+  noStroke();
+  fill(255, 0, 0);
+  rect(food.x, food.y, 1, 1);
+}
+
